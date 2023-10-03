b0d1b5574b5ed7851fae52841a23f71555c41f41
ahmet-parlak <ahmet--parlak@outlook.com>
2023-10-03 17:51:46 +0300

    Create models

---
+++ models/Project.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema({
+  title: { type: String, required: true, trim: true },
+  brief: { type: String, trim: true },
+  desctiption: { type: String, trim: true },
+  client: { type: String, trim: true },
+  category: { type: String, trim: true },
+  photo: { type: String, trim: true },
+});
+
+const Project = mongoose.model('Project', ProjectSchema);
+
+module.exports = Project;
